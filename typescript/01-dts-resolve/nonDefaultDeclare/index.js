bb7b24381f4cafff4b7d119fba9cddf392637696
Zhong Kai <apm512@qq.com>
2023-02-10 14:43:02 +0800

    引入非默认文件时，申明文件的写法

---
+++ typescript/01-dts-resolve/nonDefaultDeclare/index.js
@@ -0,0 +1,6 @@
+import add, {inc, coalese, Crass} from '@math/lib/math'
+
+console.log(add(10, 33))
+console.log(inc(3))
+console.log(coalese(3, 5, 444))
+console.log(new Crass().echo('Echo Words non default declare'))
